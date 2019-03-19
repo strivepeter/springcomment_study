bd6a981717ea469dd293110e25d8e50a115e4346
peter <1095875051@qq.com>
2019-03-19 20:48:41 +0800

    初始化项目

---
+++ src/main/java/com/peter/Test.java
@@ -0,0 +1,7 @@
+package com.peter;
+
+/**
+ * Created by Administrator on 2019/3/19.
+ */
+public class Test {
+}
---
+++ src/main/java/com/peter/bean/Person.java
@@ -0,0 +1,7 @@
+package com.peter.bean;
+
+/**
+ * Created by Administrator on 2019/3/19.
+ */
+public class Person {
+}
